5c09f5d57213e8ce1863f205d96c4d76319e750f
Zonera Javed <zonerajaved@Zoneras-MBP.fios-router.home>
2020-06-27 15:26:20 -0500

    filtering tasks

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -20,4 +20,22 @@ module.exports = {
         .then(tasks => res.status(200).send(tasks))
         .catch(error => res.status(400).send(error));
   },
+  retrieve(req, res) {
+     var {name, description, status, name, surname, userId, score} = req.query
+     var query = {}
+     if (name != null) { query.name = name };
+     if (description != null) { query.description = description};
+     if (status != null) { query.status = status };
+     if (name != null) { query.name = name };
+     if (surname != null) { query.surname = surname };
+     if (userId != null) { query.userId = userId };
+     if (score != null) { query.score = score };
+     return Task
+       .findAll({
+         where: 
+           query
+       })
+       .then(tasks => res.status(200).send(tasks))
+       .catch(error => res.status(400).send(error));
+  },
 };
